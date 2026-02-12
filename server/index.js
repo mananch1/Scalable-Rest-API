dd26061bcece597b6884fe82670a9539edc37b94
unknown <mananchahal01@gmail.com>
2026-02-13 00:25:30 +0530

    fixed swagger security policy

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
+app.use(helmet({
+    contentSecurityPolicy: false,
+}));
 
 // Swagger Config
 const swaggerOptions = {
