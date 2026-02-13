0b1500606055fc8d18abef18a466beaa8a07600f
unknown <mananchahal01@gmail.com>
2026-02-13 09:58:08 +0530

    Fixed CORS issues

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(cors({
+    origin: ['https://scalable-rest-api-frontend.vercel.app', 'http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
 app.use(helmet({
     contentSecurityPolicy: {
